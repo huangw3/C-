aa6ae9a4185370157912a6959d1252a76d6943ae
huang3 <1765942813@qq.com>
2021-01-18 11:32:07 +0800

    测试

--- game1_20210114/game.h
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#define ROW 3
-#define COL 3
- 
-//º¯ÊýÉùÃ÷
-void InitBoard(char board[ROW][COL], int row, int col);
-void DisplayBoard(char board[ROW][COL], int row, int col);
